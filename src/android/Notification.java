2928c26bdff569ef791430eb7098b82aae1eada7
Dave Funderburk <davef@edulinksys.com>
2020-08-22 02:16:31 -0700

    Update Notification.java

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -184,7 +184,7 @@ public class Notification extends CordovaPlugin {
                 dlg.setTitle(title);
                 dlg.setCancelable(true);
 //   dlg.getButton(AlertDialog.BUTTON_POSITIVE).setTextSize(TypedValue.COMPLEX_UNIT_PX, 44);
-//     dlg.getButton(dlg.BUTTON_POSITIVE).setTextColor(Color.parseColor("#f34235"));
+     dlg.getButton(dlg.BUTTON_POSITIVE).setTextColor(Color.parseColor("#f34235"));
                 dlg.setPositiveButton(buttonLabel,
                         new AlertDialog.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
